a8090f3c61c0b69d8d3dda79810284dea6df45f5
Randy Huynh <randyhuynh5815@gmail.com>
2022-03-21 16:57:18 -0700

    Portfolio completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import './portfolio.scss'
+import { portfolio } from './../../data'
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id='portfolio'>Portfolio</div>
+    <div className="portfolio" id='portfolio'>
+      <div className="portfolioContainer">
+        <h1>Portfolio</h1>
+        <div className="itemContainer">
+          {
+            portfolio.map((item) => (
+              <a className="item" href={item.link} id={item.id}>
+                <img src={item.img} alt=""></img>
+                <h3>{item.title}</h3>
+              </a>
+            ))
+          }
+        </div>
+      </div>
+      <a href="#works" id="arrow">
+        <img src="assets/down.png" alt=""></img>
+      </a>
+    </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const portfolio = [
+    {
+        id: 1,
+        title: "Mancala-AI (In Working Progress)",
+        img: "assets/mancala.jpg",
+        link: "https://github.com/RandyHuynh5815/mancala-ai"
+    },
+    {
+        id: 2,
+        title: "Sentiment Analysis (HackUCI 2022 Workshop)",
+        img: "assets/hack_workshop.PNG",
+        link: "https://github.com/aiatuci/Examples/tree/main/HackUCI2022"
+    },
+    {
+        id: 3,
+        title: "Pandemic of Lies: Truth and Deception in a Sea of Noise",
+        img: "assets/lie_pandemic.PNG",
+        link: "https://github.com/WTAWTAW-UCIMLHack2021/ucimlhack2021"
+    },
+    {
+        id: 4,
+        title: "Ultimate Tic-Tac-Toe",
+        img: "assets/ult_tic_tac_toe.png",
+        link: "https://github.com/RandyHuynh5815/ultimate-tic-tac-toe"
+    },
+    {
+        id: 5,
+        title: "BusiFind",
+        img: "assets/busifind.PNG",
+        link: "https://devpost.com/software/businessloc"
+    }
+]
